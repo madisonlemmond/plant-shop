86c3c807e7b2c0d9ff05675af2a01402e7911dfe
madisonlemmond <maddielemmond@gmail.com>
2023-10-10 09:18:00 -0700

    remove extra / from path

--- plant-shop-client/environment.prod.ts
+++ plant-shop-client/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    api: 'https://plant-shop-madison-670e93d6e5ca.herokuapp.com/'
+    api: 'https://plant-shop-madison-670e93d6e5ca.herokuapp.com'
 };
--- plant-shop-client/environment.ts
+++ plant-shop-client/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    api: 'https://plant-shop-madison-670e93d6e5ca.herokuapp.com/'
+    api: 'https://plant-shop-madison-670e93d6e5ca.herokuapp.com'
 };
